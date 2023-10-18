a78f4653ca33ed339aae89a0885421c6e30b73ba
macywahome <macywahome@gmail.com>
2023-10-17 20:07:51 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,19 +4,23 @@
  * @str: funtion paramter
  */
 
-void puts_half(char *str);
+void puts_half(char *str)
 
 {
 int i;
 int n;
 int count = 0;
 
-for (i = 0; str[i] != '0\0'; i++)
+for (i = 0; str[i] != '\0'; i++)
+{
 	count++;
 	n = (count - 1) / 2;
+}
 
 for (i = n + 1; str[i] != '\0'; i++)
+{
 	_putchar(str[i]);
 	_putchar('\n');
+}
 
 }
