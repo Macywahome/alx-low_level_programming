e836b90c197da5d09117e3d233a83f9c3387b7c7
macywahome <macywahome@gmail.com>
2023-11-05 06:28:26 -0800

    1 args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the main function
+ * @argc: function parameter
+ * @argv: functio parameter
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", *argv);
+
+	return (0);
+}
